3285311a5b91959ba58441918b1a109a16df410f
Ubuntu <ubuntu@ip-172-31-53-254.ec2.internal>
2017-07-11 20:13:06 +0000

    switched to unicorn

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'rails',        '5.0.0'
-gem 'puma',         '3.4.0'
+gem 'rails',        '5.0.1'
+gem 'unicorn'
 gem 'bootstrap-sass', '3.3.6'
 gem 'sass-rails',   '5.0.6'
 gem 'uglifier',     '3.0.0'
--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '0.0.0.0', Port: 3001)
+    end
+  end
+end
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
